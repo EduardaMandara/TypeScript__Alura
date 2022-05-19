3b4fbbf1781cd4abf80b8cd2a89f73b3ffb561ca
EduardaBezerra <eduardaf.bezerra@outlook.com>
2022-05-19 15:49:53 -0300

    entendendo pq TypeScript

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -3,10 +3,28 @@ export class Negociacao {
     #quantidade; //
     #valor; //
 
-    
     constructor (data, quantidade, valor) {
         this.#data = data;
         this.#quantidade = quantidade;
         this.#valor = valor;
-    }
-}
+    };
+
+    // Como os atributos com # a cima são privados, adicionaremos o 'get' para que eles retornem quando solicitados no App.js.
+    // Eu não posso atribuir nada a um get, só ler.
+    get data() {
+        return this.#data;
+    };
+
+    get quantdade() {
+        return this.#quantidade;
+    };
+
+    get valor() {
+        return this.#valor;
+    };
+
+    get volume() {
+        return this.#quantidade * this.#valor
+    };
+    
+};

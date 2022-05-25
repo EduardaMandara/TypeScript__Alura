a5ed01674a19e21ab39e5468e2c830c5f33950f2
EduardaBezerra <eduardaf.bezerra@outlook.com>
2022-05-25 10:26:13 -0300

    update

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -5,8 +5,6 @@ export class Negociacao {
         this._valor = valor;
     }
     ;
-    // Como os atributos com # a cima são privados, adicionaremos o 'get' para que eles retornem quando solicitados no App.js.
-    // Eu não posso atribuir nada a um get, só ler.
     get data() {
         return this._data;
     }

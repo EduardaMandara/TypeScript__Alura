7d885f7cd9b3743e151891ef22a156b790f0ca3b
EduardaBezerra <eduardaf.bezerra@outlook.com>
2022-06-07 14:50:32 -0300

    in progress

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -3,7 +3,7 @@ import { View } from './view.js';
 
 export class NegociacoesView extends View <Negociacoes> {
 
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -17,8 +17,7 @@ export class NegociacoesView extends View <Negociacoes> {
                 ${model.lista().map(negociacao => {
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat()
-                                .format(negociacao.data)}
+                            <td>${this.formatar(negociacao.data)}
                             </td>
                             <td>
                                 ${negociacao.quantidade}
@@ -33,4 +32,7 @@ export class NegociacoesView extends View <Negociacoes> {
         </table>
         `;
     };
+    formatar(data: Date): string {
+        return new Intl.DateTimeFormat().format(data);
+    }
 };
--- app/views/view.ts
+++ app/views/view.ts
@@ -10,5 +10,5 @@ export abstract class View <T> {
         this.elemento.innerHTML = template;
     }
 
-    abstract template(model: T): string;
+    protected abstract template(model: T): string;
 };
